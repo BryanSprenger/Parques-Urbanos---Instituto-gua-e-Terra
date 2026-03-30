4ae6ccba355b08cccfd32f31ac686c000dc77a30
BryanSprenger <bsprenger.curitiba@gmail.com>
2026-03-30 13:40:40 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -209,14 +209,14 @@ elif pagina == "🗺️ Mapa":
     mapa = folium.Map(location=[-24.5, -51.5], zoom_start=7, tiles=None)
 
     # Adicionando camadas base
-    folium.TileLayer('OpenStreetMap', name='Rúas (OSM)').add_to(mapa)
-    folium.TileLayer(
+        folium.TileLayer(
         tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
         attr='Esri',
         name='Satélite',
         overlay=False,
         control=True
     ).add_to(mapa)
+    folium.TileLayer('OpenStreetMap', name='Rúas (OSM)').add_to(mapa)
 
     # base municipal
     folium.GeoJson(
